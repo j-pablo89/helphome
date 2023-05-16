4408a87de22218c899b691c2ddd825b11d4f2c39
Ana Santillan <anisantillan10@gmail.com>
2023-05-16 11:33:26 -0300

    se cambio el form del login y se agrego logica

--- js/login.js
+++ js/login.js
@@ -4,3 +4,20 @@ localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
 let usuarioLs = JSON.parse(localStorage.getItem('usuarios'));
 console.log(usuarioLs)
+
+const loginForm=document.querySelector(`#loginForm`)
+loginForm.addEventListener(`submit`, (e)=>{
+    e.preventDefault()
+    const email=document.querySelector(`#email`).value
+    const password=document.querySelector(`#password`).value
+
+    const Users=JSON.parse(localStorage.getItem(`usuarios`)) || []
+    const validUsers = Users.find(user => user.email===email && user.password===password)
+    if(!validUsers){
+        return alert(`Usuario y/o contraseña incorrectos`)
+    }
+    alert(`Bienvenido ${validUsers.name}`)
+    window.location.href=`index.html`
+
+
+})
